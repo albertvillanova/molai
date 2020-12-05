33dc0f2f25a90bdd3d8fc433a456c142db143909
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2020-12-05 18:16:12 +0100

    Create dummy main module

---
+++ molai/main.py
@@ -0,0 +1,39 @@
+"""Main module."""
+
+
+def train():
+    """Train the model.
+
+    Returns
+    -------
+    None
+    """
+    print("Train the model")
+
+
+def evaluate():
+    """Evaluate the model.
+
+    Returns
+    -------
+    None
+    """
+    print("Evaluate the model")
+
+
+def predict(smile: str):
+    """Predict the property `P1` for a given smile.
+
+    Parameters
+    ----------
+    smile : str
+        Molecule smile.
+
+    Returns
+    -------
+    str
+        Prediction of the model.
+    """
+    print(f"Predict property 'P1' for smile '{smile}'")
+    prediction = "PREDICTION"
+    return prediction
