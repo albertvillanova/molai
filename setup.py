931dfe4b5add88f499eaf560448c361d64974d46
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2020-12-05 18:23:52 +0100

    Make the package installable (#9)

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+# INSTALL_REQUIRES = [
+#     "rdkit"
+# ]
+
+
+setuptools.setup(
+    name="molai",
+    version="0.0.1",
+    author="Albert Villanova",
+    author_email="albert.villanova@aiinnova.com",
+    description="Molecule AI",
+    url="https://github.com/albertvillanova/molai",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+    # install_requires=INSTALL_REQUIRES,
+)
