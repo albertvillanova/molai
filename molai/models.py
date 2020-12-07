5a469650be897feab30c34537691efebbc14f102
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2020-12-07 13:55:39 +0100

    Create models module

---
+++ molai/models.py
@@ -0,0 +1,82 @@
+"""Models module."""
+
+import tensorflow as tf
+
+from .io import write_model
+
+# Metrics for imbalanced data, better than accuracy
+METRICS = [
+    tf.keras.metrics.Precision(name='precision'),
+    tf.keras.metrics.Recall(name='recall'),
+    tf.keras.metrics.AUC(name='auc'),
+]
+
+
+def load_model(model_id="1", mode="train"):
+    """Load model and prepare it for train/evaluate/predict.
+
+    Parameters
+    ----------
+    model_id : str
+    mode : {'train', 'evaluate', 'predict'}
+        Mode the model will be used in.
+
+    Returns
+    -------
+    tf.keras.Model
+    """
+    if mode == "train":
+        model = create_model(model_id=model_id)
+    return model
+
+
+def create_model(model_id="1") -> tf.keras.Model:
+    """Create model instance and compile it.
+
+    Parameters
+    ----------
+    model_id : str
+
+    Returns
+    -------
+    tf.keras.Model
+    """
+    if model_id == "1":
+        model = tf.keras.Sequential([
+            tf.keras.layers.Dense(2, activation='relu'),
+            # tf.keras.layers.Dropout(0.2),
+            # tf.keras.layers.Dense(2, activation='relu'),
+            # tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(1, activation="sigmoid")
+        ])
+        model.compile(optimizer='adam',
+                      loss=tf.keras.losses.BinaryCrossentropy(
+                          from_logits=False),  # from_logits=True
+                      metrics=METRICS)  # ["accuracy]
+        # TODO: fix TensorFlow: METRICS (differently from "accuracy") do not
+        # work with logits (need sigmoid activation function)
+    return model
+
+
+def train_model(model, data, model_id="1"):
+    """Train the model with data.
+
+    Parameters
+    ----------
+    model : tf.keras.Model
+    data : SimpleNamespace
+    model_id : str
+
+    Returns
+    -------
+    tf.keras.Model
+    """
+    if model_id == "1":
+        model.fit(data.train.x, data.train.y,
+                  validation_data=(data.val.x, data.val.y),
+                  batch_size=128, epochs=50)
+        return model  # history,
+
+
+def save_model(model, model_id="1"):
+    write_model(model, f"model-{model_id}")
